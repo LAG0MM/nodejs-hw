d999ea21e9718611668805e75f56d0649eb34a2f
LAG0MM <166709774+LAG0MM@users.noreply.github.com>
2026-06-17 22:47:21 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.get('/notes', (req, res) => {
   res.status(200).json({  message : "Retrieved all notes" });
 });
 
-app.get('/netes/:noteId' , (req, res) => {
+app.get('/notes/:noteId' , (req, res) => {
     const { noteId } = req.params;
     res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
