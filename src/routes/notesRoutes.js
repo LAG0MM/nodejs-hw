1cdffa8f1d6fca6c90e049916ffd60e94371193c
LAG0MM <166709774+LAG0MM@users.noreply.github.com>
2026-06-27 12:50:08 +0200

    Fix route definitions for notes with noteId parameter

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -4,9 +4,9 @@ import { getAllNotes, getNoteById, createNote, deleteNote, updateNote } from "..
 const router = Router();
 
 router.get('/notes', getAllNotes);
-router.get('/notes:noteId', getNoteById);
+router.get('/notes/:noteId', getNoteById);
 router.post('/notes', createNote);
-router.patch('/notes:noteId', updateNote);
-router.delete('/notes:noteId', deleteNote);
+router.patch('/notes/:noteId', updateNote);
+router.delete('/notes/:noteId', deleteNote);
 
 export default router;
